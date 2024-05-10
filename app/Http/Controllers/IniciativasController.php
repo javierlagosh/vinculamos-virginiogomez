9fbd27ff552f290096b5b2a0a6b00a36647caa55
mcartes <44407924+mcartes@users.noreply.github.com>
2024-05-10 10:48:56 -0400

    Update IniciativasController.php

--- app/Http/Controllers/IniciativasController.php
+++ app/Http/Controllers/IniciativasController.php
@@ -1230,7 +1230,7 @@ class IniciativasController extends Controller
             IniciativasComunas::where('inic_codigo', $inic_codigo)->delete();
             return redirect()->back()->with('comuError', 'Ocurrió un error durante el registro de las comunas, intente más tarde.')->withInput();
         }
-
+        return 1;
         return redirect()->route('admin.editar.paso2', $inic_codigo)->with('exitoPaso1', 'Los datos de la iniciativa se actualizaron correctamente');
 
     }
