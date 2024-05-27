942516afa4f71bdd2d4c11810d3ba06fd18856f0
mcartes <44407924+mcartes@users.noreply.github.com>
2024-05-27 17:41:40 -0400

    Update INVI.js

--- public/js/admin/iniciativas/INVI.js
+++ public/js/admin/iniciativas/INVI.js
@@ -110,6 +110,8 @@ function calcularIndice(inic_codigo) {
             (isNaN(parseInt(coberturaExt[0].total_externos))
               ? 0
               : parseInt(coberturaExt[0].total_externos));
+
+            ptje_externos = Math.round((coberturaExt[0].total_externos_final* 100 ) / coberturaExt[0].total_externos);
         }
 
         if (dividendo == null) {
@@ -166,7 +168,7 @@ function calcularIndice(inic_codigo) {
       $("#mecanismo-puntaje").text(mecanismo_puntaje);
       $("#mecanismo-nombre").text(mecanismo[0].meca_nombre);
       $("#frecuencia-puntaje").text(frecuencia_puntaje);
-      $("#cobertura-puntaje").text(cobertura_puntaje + "dividendo: " + dividendo + " divisor: " + divisor + " - " + coberturaExt[0].total_externos);
+      $("#cobertura-puntaje").text(cobertura_puntaje + "dividendo: " + dividendo + " divisor: " + divisor + " - " + ptje_externos);
       $("#resultados-puntaje").text(resultados_puntaje);
       $("#evaluacion-puntaje").text(evaluacion_puntaje);
       $("#valor-indice").text(indice);
