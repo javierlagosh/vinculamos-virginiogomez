afb0a5070429eee2fb4dde4b5f045133ceaf1c5d
mcartes <44407924+mcartes@users.noreply.github.com>
2024-05-27 17:10:58 -0400

    Update INVI.js

--- public/js/admin/iniciativas/INVI.js
+++ public/js/admin/iniciativas/INVI.js
@@ -118,7 +118,7 @@ function calcularIndice(inic_codigo) {
         if (divisor == null || divisor == 0 || dividendo == 0) {
           cobertura_puntaje = 0;
         } else {
-          cobertura_puntaje = Math.round((dividendo + divisor / 2) / divisor);
+          cobertura_puntaje = Math.round(100 - (dividendo + divisor / 2));
           if (cobertura_puntaje > 100) {
             cobertura_puntaje = 100;
           }
